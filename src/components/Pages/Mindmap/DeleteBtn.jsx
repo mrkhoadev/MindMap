f21086c26288d2e417d699b2c704883d49c0e4c6
Anh Khoa <mr.mrkhoacoding@gmail.com>
2023-12-28 00:04:07 +0700

    test

--- src/components/Pages/Mindmap/DeleteBtn.jsx
+++ src/components/Pages/Mindmap/DeleteBtn.jsx
@@ -10,7 +10,7 @@ export default function DeleteBtn({ id, name, deleteMutation }) {
   const handleDelete = async () => {
     if (typeof document !== 'undefined') {
       const {confirm, error} = await import('alertifyjs');
-      confirm("Xóa MindMap này?",`'Bạn có chắc chắn muốn xóa "${name}"?'`,
+      confirm(`Xóa "${name}"?`,'Bạn có chắc chắn muốn xóa MindMap này không?',
       function(){
         dispatch(
           setIsLoading(true)
